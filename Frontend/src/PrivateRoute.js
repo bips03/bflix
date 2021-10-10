b4cf998cca0616ae32cd177af1ab8d52fe8f24b0
bips03 <biplabps2@gmail.com>
2021-10-11 01:56:33 +0530

    final

--- Frontend/src/PrivateRoute.js
+++ Frontend/src/PrivateRoute.js
@@ -5,11 +5,12 @@ import { Redirect, Route } from "react-router-dom";
 function PrivateRoute({ component: Component, ...rest }) {
   const user = useSelector((state) => state.user);
   const payment = useSelector((state) => state.payment);
+  const check = Object.keys(payment).length > 2
   return (
     <Route
       {...rest}
       render={(props) => {
-        return user ? payment? <Component {...props} /> : <Redirect to = '/payment' />  : <Redirect to="/login" />;
+        return user ? check? <Component {...props} /> : <Redirect to = '/payment' />  : <Redirect to="/login" />;
       }}
     ></Route>
   );
